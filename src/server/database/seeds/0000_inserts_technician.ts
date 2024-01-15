578d927b0ec9a4b89305a9d8dce54490d0b36da7
BrenoMatheus <breno.moj99@gmail.com>
2024-01-15 16:27:55 -0300

    fix: added seed for population table technician

---
+++ src/server/database/seeds/0000_inserts_technician.ts
@@ -0,0 +1,38 @@
+import { Knex } from "knex";
+import { ETableNames } from "../ETableNames";
+
+export const seed = async (knex: Knex) => {
+  const [{ count }] = await knex(ETableNames).count<[{ count: number }]>(
+    "* as count"
+  );
+  if (!Number.isInteger(count) || Number(count) > 0) return;
+
+  const technicianToInsert = technician.map((technician) => ({
+    name: technician.name,
+    email: technician.email,
+    category: technician.category,
+    description: technician.description,
+  }));
+  await knex(ETableNames.technician).insert(technicianToInsert);
+};
+
+const technician = [
+  {
+    name: "Teste",
+    email: "test@mail.com",
+    category: "mecanico",
+    description: "Este é o teste de mecanico",
+  },
+  {
+    name: "Teste",
+    email: "test1@mail.com",
+    category: "eletricista",
+    description: "Este é o teste de eletricista",
+  },
+  {
+    name: "Teste",
+    email: "test2@mail.com",
+    category: "TTT",
+    description: "Este é o teste de TTT",
+  },
+];
