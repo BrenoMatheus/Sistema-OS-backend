f0760b69913cbade9b09beea04b03eeb8ff98995
BrenoMatheus <breno.moj99@gmail.com>
2024-01-15 12:37:35 -0300

    fix: add router of create technician

--- src/server/controllers/technicians/Create.ts
+++ src/server/controllers/technicians/Create.ts
@@ -6,22 +6,25 @@ import { TechnicianProvider } from "../../database/providers/technicians";
 import { ITechnician } from "../../database/models";
 import { validation } from "../../shared/middleware";
 
-//omit 'id' because we are not going to use it in creation 
+//omit 'id' because we are not going to use it in creation
 interface IBodyProps extends Omit<ITechnician, "id"> {}
 
 // schema for validation data
 export const createValidation = validation((getSchema) => ({
   body: getSchema<IBodyProps>(
     yup.object().shape({
-        name: yup.string().required().min(3).max(70),
-        email: yup.string().required().email(),
-        category: yup.string().required().min(3).max(70),
-        description: yup.string().optional(),
+      name: yup.string().required().min(3).max(70),
+      email: yup.string().required().email(),
+      category: yup.string().required().min(3).max(70),
+      description: yup.string().optional(),
     })
   ),
 }));
 
-export const create = async (req: Request<{}, {}, IBodyProps>, res: Response) => {
+export const create = async (
+  req: Request<{}, {}, IBodyProps>,
+  res: Response
+) => {
   const result = await TechnicianProvider.create(req.body);
 
   if (result instanceof Error) {
--- src/server/routes/index.ts
+++ src/server/routes/index.ts
@@ -1,5 +1,19 @@
 import { Router } from "express";
 
+import {
+  TechnicianController,
+} from "./../controllers";
+
 const router = Router();
+
+router.get("/", (_, res) => {
+  return res.send("Olá, DEV!");
+});
+
+router.post(
+  "/technicians",
+  TechnicianController.createValidation,
+  TechnicianController.create
+);
 
 export { router };
