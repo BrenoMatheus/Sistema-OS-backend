f6e88482ed40e3d21f5850c966d318e1a3595e3e
BrenoMatheus <breno.moj99@gmail.com>
2024-01-16 16:38:51 -0300

    feat: added Interace for item of line

--- src/server/database/knex/@types/knex.d.ts
+++ src/server/database/knex/@types/knex.d.ts
@@ -1,9 +1,10 @@
-import { ITechnician } from "../../models/technician";
+import { IEquipment, IItem, IItemofLine, ITechnician } from "../../models";
 
 declare module "knex/types/tables" {
   interface Tables {
     technician: ITechnician;
     equipment: IEquipment;
     item: IItem;
+    itemofLine: IItemofLine;
   }
 }
