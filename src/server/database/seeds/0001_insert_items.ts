d8fe7960fa7100c409abb44f80ff34914942ed3a
BrenoMatheus <breno.moj99@gmail.com>
2024-02-03 17:55:50 -0300

    fix: add names tables for condition of seed

--- src/server/database/seeds/0001_insert_items.ts
+++ src/server/database/seeds/0001_insert_items.ts
@@ -2,7 +2,7 @@ import { Knex } from "knex";
 import { ETableNames } from "../ETableNames";
 
 export const seed = async (knex: Knex) => {
-  const [{ count }] = await knex(ETableNames).count<[{ count: number }]>(
+  const [{ count }] = await knex(ETableNames.item).count<[{ count: number }]>(
     "* as count"
   );
   if (!Number.isInteger(count) || Number(count) > 0) return;
